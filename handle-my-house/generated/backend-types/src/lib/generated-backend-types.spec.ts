8e3e5a0f9fe9a91647101adc8a645a667952a379
YuriBispo <yuribispomartins@hotmail.com>
2023-12-22 19:49:05 -0300

    chore: removing dotnet project

--- handle-my-house/generated/backend-types/src/lib/generated-backend-types.spec.ts
+++
@@ -1,7 +0,0 @@
-import { generatedBackendTypes } from './generated-backend-types';
-
-describe('generatedBackendTypes', () => {
-  it('should work', () => {
-    expect(generatedBackendTypes()).toEqual('generated-backend-types');
-  });
-});
